23c36160a65239536f13f478d59de7e9c3dd4880
David <drs@a.recife.ifpe.edu.br>
2019-11-26 00:03:51 +0000

    alimentando dataset

--- src/createDataset.py
+++ src/createDataset.py
@@ -12,36 +12,39 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-emotions = {1:"tristeza", 2:"raiva", 3:"frustração", 4:"alegria", 5:"humor", 6:"amor", 7:"esperança", 8:"entusiasmo", 9:"neutro"}
+emotions = {1:"tristeza", 2:"raiva", 3:"frustração", 4:"alegria", 5:"humor", 6:"amor", 7:"esperança", 8:"entusiasmo", 9:"neutro", 10:"dúvida", 11:"ironia" , 12:"saudade"}
 data = []
 
-query = "c"
+query = "globolóides"
 maxCount = 10
 max_id = -1
 count = 0
 print(emotions)
-print("\n\n")
+print("\n")
+
+
 while count < maxCount:
     if max_id <= 0:
-        searched_tweets = api.search(q=query+" -filter:retweets",lang="pt-br" ,tweet_mode='extended', count=maxCount)
+        searched_tweets = api.search(q=query+" -filter:retweets",lang="pt-br" ,tweet_mode='extended', count=maxCount*5)
     else:
-        searched_tweets = api.search(q=query+" -filter:retweets", lang="pt-br" ,tweet_mode='extended', count=maxCount, max_id=str(max_id - 1))
+        searched_tweets = api.search(q=query+" -filter:retweets", lang="pt-br" ,tweet_mode='extended', count=maxCount*5, max_id=str(max_id - 1))
     if not searched_tweets:
         print("tem nada aq mona") 
         break
     else:
-        
         for tweet in searched_tweets:
-            if (tweet.place is not None) and (count < maxCount):
-                count += 1
+            if (count < maxCount):
                 text = (json.dumps(tweet._json['full_text'], sort_keys=True, indent=4, ensure_ascii=False).encode('utf8')).decode()
-                print(text)
-                index = int(input("classifique: "))
-                aux = [text, emotions[index]]
-                data.append(aux)
-                print(tweet.id)
+                if not "https://" in text:
+                    count += 1
+                    print(text)
+                    index = int(input("classifique: "))
+                    aux = [text, emotions[index]]
+                    data.append(aux)
+                    print(tweet.id)
 
     max_id = searched_tweets[-1].id
+
 df = pd.DataFrame(data, columns=['Texto', 'Emoção'])
 print(df)
 with open('EmotionsDataset.csv', 'a') as dataset:
